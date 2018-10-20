9b35864972d6fffac410b1d790964f95d605939c
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2018-10-20 16:57:01 +0100

    refactoring: add default temperature

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -4,7 +4,8 @@ function Thermostat() {
   this.MINIMUM_TEMPERATURE = 10;
   this.MAX_LIMIT_PSM_ON = 25;
   this.MAX_LIMIT_PSM_OFF = 32;
-  this.temperature = 20;
+  this.DEFAULT_TEMPERATURE = 20
+  this.temperature = this.DEFAULT_TEMPERATURE;
   this.powerSavingMode = true;
 }
 
@@ -50,5 +51,5 @@ Thermostat.prototype.isMaximumTemperature = function () {
 };
 
 Thermostat.prototype.resetTemperature = function () {
-  this.temperature = 20;
+  this.temperature = this.DEFAULT_TEMPERATURE;
 };
