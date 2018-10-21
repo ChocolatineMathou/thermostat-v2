2163de8d46f30e3e8efa5c2fdafd17aed92b19b4
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2018-10-21 13:04:40 +0100

    feat: add up and down to interfance

--- interface.js
+++ interface.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
   const thermostat = new Thermostat();
   $('#temperature').text(thermostat.temperature);
+
+  $('#up').on('click', function() {
+    thermostat.up();
+    $('#temperature').text(thermostat.temperature);
+  })
+
+  $('#down').on('click', function() {
+    thermostat.down();
+    $('#temperature').text(thermostat.temperature);
+  })
 })
